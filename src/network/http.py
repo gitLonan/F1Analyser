de445f8626da2c5fe7f007215ceef93adc056783
Lazar <sc2lazar@gmail.com>
2025-03-24 13:21:43 +0100

    added check if the domain name is correct

--- src/network/http.py
+++ src/network/http.py
@@ -1,23 +1,30 @@
 import ssl
 import socket
-
+import sys
 
 
 class HttpRequest():
     global port
     port = 443
-    
 
     def establish_Connection(host):
         try:
             raw_socket = socket.create_connection((host, port), timeout=5)
             return raw_socket
-        except Exception:
-            print("Something is wrong")
+        except socket.gaierror as error:
+           print(f"Error: {error.errno}, Message: {error.strerror}")
+           sys.exit()
 
     def ssl_wrapper(host, socket):
         secure = ssl.create_default_context().wrap_socket(socket, server_hostname=host)
         return secure
+    
+    def status_code(response):
+        splited_response = response.split("\r\n")
+        code = splited_response[0].split(" ")[1]
+        text = splited_response[0].split(" ")[2]
+        print(code, text)
+        return code, text
         
 
 
