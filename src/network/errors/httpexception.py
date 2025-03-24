26007366ea18d49f7242be152707ee8cfb9a479d
Lazar <sc2lazar@gmail.com>
2025-03-24 13:21:24 +0100

    added exception if http codes is not 200

---
+++ src/network/errors/httpexception.py
@@ -0,0 +1,36 @@
+import socket
+
+class HttpException(Exception):
+
+    @classmethod
+    def exception(cls, code, text, host):
+        if 400 <= code < 500:
+            return ClientError.error(code, text, host)
+        elif 500 <= code < 600:
+             return ServerError.error(code, text, host)
+    
+
+class ClientError(HttpException):
+    @classmethod
+    def error(cls, code, text, host):
+        message = f"""
+            Error: {code} {text}
+            Connecting to: {host}
+
+            
+            """
+        return cls(message)
+
+class ServerError(HttpException):
+    @classmethod
+    def error(cls, code, text, host):
+        message = f"""
+            Error: {code} {text}
+            Connecting to: {host}
+
+            Check if your api is correct, maybe there is some kind of typo
+            """
+        return cls(message)
+    
+
+
