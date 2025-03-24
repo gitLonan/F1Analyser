13c7d9311c7179bafb70a6a290bbf6bf8b8df862
Lazar <sc2lazar@gmail.com>
2025-03-24 13:22:52 +0100

    added new api paths

--- src/network/api.py
+++ src/network/api.py
@@ -1,15 +1,22 @@
+#My classes
 from src.network.http import HttpRequest
+from src.network.errors.httpexception import HttpException
+#My func
 from urllib.parse import quote
+
+
 from enum import Enum
 import json
 
 class API(Enum):
     DRIVERS = "/v1/drivers?"
     LAPS = "/v1/laps?"
+    POSITION = "/v1/position?"
+    STINTS = "/v1/stints?"
 
-class ApiCommunication():
+class ApiCommunication:
 
-    def get_api(host, api=None,attributes=[]) -> json:
+    def get_api(host: str, api: str =None,attributes: list[str]=[]) -> json:
         """ 
             Args:
                 host (str) - websites domain name, example api.openf1.org
@@ -25,26 +32,35 @@ class ApiCommunication():
         request = f"GET {api} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
         connection = HttpRequest.establish_Connection(host)
         secure_con = HttpRequest.ssl_wrapper(host, connection)
+        
         secure_con.sendall(request.encode())
         response = b""
         while True:
-            chunk = secure_con.recv(4096)
+            chunk = secure_con.recv(4096) #industry standard for size of chunks to communicate with
             if not chunk:
                 break
             response += chunk
+        
         response_text = response.decode()
-        print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSsss", response_text)
+        code, text = HttpRequest.status_code(response_text)
+        code = int(code)
+        if code != 200:
+            raise HttpException.exception(code, text, host)
+            
         secure_con.close()
         return response_text
 
-    def extractingJson(data) -> json:
+class JsonHandling:
+    def extractingJson(data: str) -> json:
+        """Extracting json part of the Http get request, disregarding header"""
         parts = data.split("\r\n\r\n", 1)
         json_part = parts[1].strip()
         json_response = json.loads(json_part)
         return json_response
     
-    def pretty_json(ugly_json):
+    def pretty_json(ugly_json: json) -> json:
         return print(json.dumps(ugly_json, indent=4))
+        
 
 
     
