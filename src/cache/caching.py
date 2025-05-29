723be978e43bc5c87244d5ab7afe48576c064985
Lazar <sc2lazar@gmail.com>
2025-05-30 00:22:22 +0200

    ways to cache api calls

--- src/cache/caching.py
+++ src/cache/caching.py
@@ -10,7 +10,7 @@ class CacheAPI:
             file.write(pretty_json)
             
     def cache_tracks_data(json_data: json, meeting_key) -> None:
-        with open(f"data/cached_calls/sessions/session_{meeting_key}_tracks_data.json", "w") as file:
+        with open(f"data/cached_calls/sessions/meeting_{meeting_key}_tracks_data.json", "w") as file:
             pretty_json = JsonHandling.pretty_json(json_data)
             file.write(pretty_json)
 
@@ -23,8 +23,19 @@ class CacheAPI:
         with open(f"data/cached_calls/stints/{session_key}/driver_number_{driver}.json", "w") as file:
             pretty_json = JsonHandling.pretty_json(json_data)
             file.write(pretty_json)
+            
     def cache_laps_data(json_data: json, session_key, driver) -> None:
         with open(f"data/cached_calls/laps/{session_key}/driver_number_{driver}.json", "w") as file:
+            pretty_json = JsonHandling.pretty_json(json_data)
+            file.write(pretty_json)
+            
+    def cache_car_data(json_data: json, session_key, driver) -> None:
+        with open(f"data/cached_calls/car_data/{session_key}/driver_number_{driver}.json", "w") as file:
+            pretty_json = JsonHandling.pretty_json(json_data)
+            file.write(pretty_json)
+
+    def cache_positions(json_data: json, session_key, driver) -> None:
+        with open(f"data/cached_calls/positions/{session_key}/drivers_position_{driver}.json", "w") as file:
             pretty_json = JsonHandling.pretty_json(json_data)
             file.write(pretty_json)
     
@@ -38,7 +49,8 @@ class CacheAPI:
             print(list_of_files)
         except FileNotFoundError:
             os.makedirs((f"data/cached_calls/{folder}"), exist_ok=True)
-            list_of_files = os.listdir(dir_path_content)
+
+        list_of_files = os.listdir(dir_path_content)
 
         print(list_of_files)
         for file in list_of_files:
@@ -49,3 +61,18 @@ class CacheAPI:
                 return True
         print("We dont have that file")
         return False
+    
+    # def exists_parent_folder(folder: str, amount: int) -> bool:
+    #     """
+    #         Args: 
+    #             folder (str) - name of the 'parent' folder which holds relevant session keys
+    #         Returns:
+    #             returns bool where false means folder didnt exist and that it created it
+    #     """
+    #     folder_path = f"data/cached_calls/{folder}"
+    #     list_of_files = os.listdir(folder_path)
+
+    #     if not os.path.isdir(folder_path) or len(list_of_files) < 20:
+    #         os.makedirs((f"data/cached_calls/{folder}"), exist_ok=True)
+    #         return False
+    #     return True
